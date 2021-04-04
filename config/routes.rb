d76c52acf692b83c20fc7a351b93d11179692e6b
Héctor Tello <hectortllo@gmail.com>
2021-04-04 00:10:24 -0600

    configuración de devise finalziada

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
   resources :rooms
   root to: "pages#home"
   get 'pages/home'
