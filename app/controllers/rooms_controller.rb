08261c901b8a9921defa19b24f59a2e958154515
Héctor Tello <hectortllo@gmail.com>
2021-04-11 00:36:17 -0600

    Create action finished

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,8 +8,38 @@ class RoomsController < ApplicationController
   def show
   end
 
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to rooms_path,
+                      notice: 'Room creado correctamente' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   private
   def set_room
     @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:id,
+                                :length,
+                                :width,
+                                :image,
+                                :hotel_id,
+                                :taken,
+                                :description)
+                          .with_defaults(hotel_id: 1,
+                                        image: "http://placehold.it/700x400",
+                                        taken: 0)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,12 @@
 class Room < ApplicationRecord
   belongs_to :hotel
+
+  validates :length,
+            :width,
+            :description,
+            :hotel_id,
+            :image,
+            presence: true
+
+  validates :length, :width, numericality: { only_float: true}
 end
