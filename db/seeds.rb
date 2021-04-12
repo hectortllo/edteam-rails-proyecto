6da7f3abd606288a172ead3e2b1e212b9e6b5e7d
Héctor Tello <hectortllo@gmail.com>
2021-04-11 23:44:17 -0600

    Proyecto casi finalizado

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,3 @@ Hotel.create!(
 )
 
 puts "Hotel creado"
-
-25.times do
-  Room.create(
-    length: rand(10),
-    width: rand(10),
-    taken: rand(2),
-    image: "http://placehold.it/700x400",
-    description: Faker::Lorem.sentence(word_count: 100),
-    hotel_id: Hotel.first.id
-  )
-end
-
-puts "Habitaciones creadas"
